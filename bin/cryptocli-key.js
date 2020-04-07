c60a6a852b5f875edb05f38c648ad26783800f09
Devin <devdhera@gmail.com>
2020-04-07 12:03:09 +0530

    Base setup for SHOW and REMOVE

--- bin/cryptocli-key.js
+++ bin/cryptocli-key.js
@@ -5,4 +5,14 @@ program
   .description('Set API Key -- Get at https://nomics.com')
   .action(() => console.log('Hello from Set'));
 
+program
+  .command('show')
+  .description('Show API Key')
+  .action(() => console.log('Hello from Show'));
+
+program
+  .command('remove')
+  .description('Remove API Key')
+  .action(() => console.log('Hello from Remove'));
+
 program.parse(process.argv);
