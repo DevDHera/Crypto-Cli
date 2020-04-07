c45198be41f85a11dc8b99c91dd1435670c101cb
Devin <devdhera@gmail.com>
2020-04-07 11:40:20 +0530

    Configured Key Command

--- bin/cryptocli.js
+++ bin/cryptocli.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Hello from Crypto Cli');
+const program = require('commander');
+const pkg = require('../package.json');
+
+program
+  .version(pkg.version)
+  .command('key', 'Manage API Key -- https://nomics.com')
+  .parse(process.argv);
