7b05151c7a875ec84bae94e3dc1fd794270061c8
Devin <devdhera@gmail.com>
2020-04-07 19:02:52 +0530

    Completed Crypto API class

--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,20 @@
+const KeyManager = require('../lib/KeyManager');
+const CryptoAPI = require('../lib/CryptoAPI');
+
 const check = {
-  price(cmd) {
-    console.log('Hello from Price');
+  async price(cmd) {
+    try {
+      const keyManager = new KeyManager();
+      const key = keyManager.getKey();
+
+      const api = new CryptoAPI(key);
+
+      const priceOutputData = await api.getPriceData(cmd.coin, cmd.cur);
+
+      console.log(priceOutputData);
+    } catch (error) {
+      console.error(error.message.red);
+    }
   },
 };
 
