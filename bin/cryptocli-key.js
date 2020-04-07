dd0aaa9822fc547b21e547d24ca9e58081b0e808
Devin <devdhera@gmail.com>
2020-04-07 17:02:37 +0530

    Seperated logic from key mapper

--- bin/cryptocli-key.js
+++ bin/cryptocli-key.js
@@ -1,18 +1,13 @@
 const program = require('commander');
+const key = require('../commands/key');
 
 program
   .command('set')
   .description('Set API Key -- Get at https://nomics.com')
-  .action(() => console.log('Hello from Set'));
+  .action(key.set);
 
-program
-  .command('show')
-  .description('Show API Key')
-  .action(() => console.log('Hello from Show'));
+program.command('show').description('Show API Key').action(key.show);
 
-program
-  .command('remove')
-  .description('Remove API Key')
-  .action(() => console.log('Hello from Remove'));
+program.command('remove').description('Remove API Key').action(key.remove);
 
 program.parse(process.argv);
---
+++ commands/key.js
@@ -0,0 +1,13 @@
+const key = {
+  set() {
+    console.log('Hello from Set');
+  },
+  show() {
+    console.log('Hello from Show');
+  },
+  remove() {
+    console.log('Hello from Remove');
+  },
+};
+
+module.exports = key;
