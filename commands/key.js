e83cdbf525dd8d6465f34fe2e4ba88a2c70f0717
Devin <devdhera@gmail.com>
2020-04-07 17:22:10 +0530

    Configured key set method

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,24 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+const KeyManager = require('../lib/KeyManager');
+
 const key = {
-  set() {
-    console.log('Hello from Set');
+  async set() {
+    const keyManager = new KeyManager();
+    const input = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'key',
+        message: 'Enter API Key '.green + 'https://nomics.com',
+        validate: 
+      },
+    ]);
+
+    const key = keyManager.setKey(input.key);
+
+    if (key) {
+      console.log('API Key Set'.blue);
+    }
   },
   show() {
     console.log('Hello from Show');
